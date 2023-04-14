2b82b0f9a607a42673e530df1569ac8f5e9f13f0
Edfram-Guerra <edfram.guerra@gmail.com>
2023-04-14 16:59:45 -0300

    implementado modelagem (schema sql) e  configurações do banco de dados e variavéis de ambiente

--- src/connection/database.js
+++ src/connection/database.js
@@ -1,1 +1,17 @@
-//conexão banco de dados
+require('dotenv').config();
+const pg = require('pg');
+
+pg.defaults.ssl = true;
+
+const connection = require('knex')({
+    client: process.env.DB_CLIENT,
+    connection: {
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASS,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST
+    }
+})
+
+module.exports = connection;
