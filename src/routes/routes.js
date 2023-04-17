d12b1fb4ca43f2802d6af45271e63ec441759798
Shylton Santana <shyltondiego0@gmail.com>
2023-04-16 22:17:23 -0300

    Implementação do cadastro do usuários

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
+const uniqueEmail = require('../middlewares/uniqueEmail');
+const { createUser } = require('../controllers/userController');
 
 const routes = Router();
 
 routes.get('/', (req, res) => { res.json('server is running') });
 
+routes.post('/usuario', uniqueEmail, createUser);
+
 routes.use(() => { }); // intermediário de autenticação
+
 
 module.exports = routes;
