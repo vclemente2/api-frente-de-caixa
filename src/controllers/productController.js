f8b6c92be55a9715a5e0f9ca1843cf90018b1e09
Edfram-Guerra <edfram.guerra@gmail.com>
2023-04-27 19:24:03 -0300

    comentarios excluidos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,7 +4,6 @@ const { productRepository } = require('../repositories/ProductRepository');
 const createProduct = async (req, res) => {
     const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
 
-    //validações via schema
     const product = await productRepository.create({ descricao, quantidade_estoque, valor, categoria_id })
 
     if (!product) throw new InternalServerError('Não foi possível criar o produto')
