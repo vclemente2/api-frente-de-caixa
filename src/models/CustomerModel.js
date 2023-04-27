6e8cd463404fa05dde405c0a579c6c331d34d0de
Vinicius Bastos <vclemente2@hotmail.com>
2023-04-26 23:50:06 -0300

    Implementação do model e repository de clientes.

---
+++ src/models/CustomerModel.js
@@ -0,0 +1,46 @@
+const { DataTypes, Model } = require('sequelize')
+
+class Customer extends Model {
+    static init(connection) {
+        super.init({
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true
+            },
+            cpf: {
+                type: DataTypes.STRING(11),
+                allowNull: false,
+                unique: true
+            },
+            cep: {
+                type: DataTypes.STRING,
+            },
+            rua: {
+                type: DataTypes.STRING,
+            },
+            numero: {
+                type: DataTypes.STRING,
+            },
+            bairro: {
+                type: DataTypes.STRING,
+            },
+            cidade: {
+                type: DataTypes.STRING,
+            },
+            estado: {
+                type: DataTypes.STRING,
+            }
+        },
+            {
+                sequelize: connection,
+                modelName: 'clientes'
+            })
+    }
+}
+
+module.exports = Customer
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express')
+
 const { verifyLoggedUser } = require('../middlewares/authMiddleware')
+
 const userRoutes = require('./userRoutes')
 const categoryRoutes = require('./categoryRoutes')
 const authRoutes = require('./authRoutes')
 const productRoutes = require('./productRoutes')
+const customerRoutes = require('./customerRoutes')
 
 const routes = Router()
 
@@ -15,5 +18,6 @@ routes.use(verifyLoggedUser)
 
 routes.use('/usuario', userRoutes.private)
 routes.use('/produto', productRoutes)
+routes.use('/cliente', customerRoutes)
 
 module.exports = routes
