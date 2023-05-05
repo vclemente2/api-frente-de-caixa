fe871ea721eb6007c2ec2585dcd6d0170db72ca1
Vinicius Bastos <vclemente2@hotmail.com>
2023-05-05 19:29:08 -0300

    Ajuste da conexão para deploy.

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -7,7 +7,6 @@ module.exports = {
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
-    native: true,
     ssl: true,
     dialectOptions: {
         ssl: true
