896648cba74322f3387869b4082df7969b349764
Vinicius Bastos <vclemente2@hotmail.com>
2023-05-14 01:20:56 -0300

    Implementação da validação e atualização do estoque no enpoint de cadastro de pedido.

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,45 +1,47 @@
 const { productRepository } = require('../repositories/ProductRepository')
 const { orderRepository } = require('../repositories/OrderRepository')
 const { orderProductRepository } = require('../repositories/OrderProductRepository')
-const { Op } = require('sequelize')
+const { Sequelize, Op } = require('sequelize')
 const InternalServerError = require('../errors/InternalServerError')
 const Customer = require('../models/CustomerModel')
 const NotFoundError = require('../errors/NotFoundError')
+const ConflictError = require('../errors/ConflictError')
 
 const createOrder = async (req, res) => {
     const { cliente_id, observacao, pedido_produtos } = req.body
 
     /*
-
-    3 - Validar se existe a quantidade em estoque de cada produto existente dentro do array, de acordo com a quantidade informada no corpo (body) da requisição.
-
-    4 - O pedido deverá ser cadastrado, apenas, se todos os produtos estiverem validados.
-
     5 - Enviar e-mail para o cliente notificando que o pedido foi efetuado com sucesso.
     */
 
     const idProducts = pedido_produtos.map((product) => { return product.produto_id })
 
-    const products = await productRepository.findAll({ id: { [Op.in]: idProducts } }, { attributes: ['id', 'valor', 'descricao', 'quantidade_estoque'] })
+    const requiredProducts = await productRepository.findAll({ id: { [Op.in]: idProducts } }, { attributes: ['id', 'valor', 'descricao', 'quantidade_estoque'] })
 
-    if (products.length !== idProducts.length) throw new NotFoundError('Um ou mais produtos inexistente(s)')
+    if (requiredProducts.length !== idProducts.length) throw new NotFoundError('Um ou mais produto(s) inexistente(s)')
 
     const productData = pedido_produtos.map((product => {
         let data;
-        products.forEach(item => {
-            if (item.id === product.produto_id) {
-                data = { descricao: item.descricao, ...product, valor_produto: item.valor, }
+        requiredProducts.forEach(requiredProduct => {
+            const { id, descricao, quantidade_estoque, valor } = requiredProduct
+            if (id === product.produto_id) {
+                if (quantidade_estoque < product.quantidade_produto) {
+                    throw new ConflictError(
+                        `O produto ${descricao} não possui estoque suficiente. Quantidade solicitada ${product.quantidade_produto}, quantidade em estoque ${quantidade_estoque}`
+                    )
+                }
+                data = { descricao, ...product, valor_produto: valor, }
             }
         })
         return data
     }))
 
-    const totalValue = productData.reduce((accum, current) => {
+    const totalOrderValue = productData.reduce((accum, current) => {
         accum += current.valor_produto * current.quantidade_produto
         return accum
     }, 0)
 
-    const order = await orderRepository.create({ cliente_id, observacao, valor_total: totalValue })
+    const order = await orderRepository.create({ cliente_id, observacao, valor_total: totalOrderValue })
 
     if (!order) throw new InternalServerError('Não foi possível cadastrar o pedido')
 
@@ -53,6 +55,15 @@ const createOrder = async (req, res) => {
         await orderRepository.delete({ id: order.id })
         throw new InternalServerError('Não foi possível cadastrar o pedido')
     }
+
+    orderProduct.forEach(async ({ produto_id, quantidade_produto }) => {
+        await productRepository.update(
+            {
+                quantidade_estoque: Sequelize.literal(`quantidade_estoque - ${quantidade_produto}`)
+            },
+            { id: produto_id }
+        )
+    })
 
     const orderData = await orderRepository.findOne({ id: order.id }, { include: { model: Customer, as: 'cliente' } })
 
